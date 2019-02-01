ecf581c919cf748771db7fc8d3252e73238e26ba
Akili Humphrey <akilihumphrey@gmail.com>
2019-02-01 01:55:37 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -25,8 +25,8 @@ ActiveRecord::Schema.define(version: 3) do
   create_table "costumes", force: :cascade do |t|
     t.string "name"
     t.float "price"
-    t.string "size"
-    t.string "image_url"
+    t.integer "size"
+    t.string "url"
   end
 
   create_table "haunted_houses", force: :cascade do |t|
