34190854450fc4a2c4441b4e83287e1fc4fd1397
Akili Humphrey <akilihumphrey@gmail.com>
2019-02-01 01:54:03 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -3,8 +3,8 @@ class CreateCostumes < ActiveRecord::Migration[5.1]
       create_table :costumes do |t|
         t.string  :name
         t.float :price
-        t.integer :size
-        t.string  :url
+        t.string :size
+        t.string  :image_url
       end
   end
 end
